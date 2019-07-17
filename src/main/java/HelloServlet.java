1228dc21a6eb770a57aba531db6c8aa83ef1d331
jiangtingfeng <2315507963@qq.com>
2019-07-17 09:12:39 +0800

    test111

---
+++ src/main/java/HelloServlet.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.util.Date;
+
+public class HelloServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+            try{
+                response.getWriter().println("hello servlet");
+                response.getWriter().println(new Date().toLocaleString());
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+    }
+}
