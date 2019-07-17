2c94d144f04b9b3f398546a00b283810e3cea428
jiangtingfeng <2315507963@qq.com>
2019-07-17 09:15:39 +0800

    test111

--- src/main/java/HelloServlet.java
+++ src/main/java/HelloServlet.java
@@ -8,7 +8,7 @@ public class HelloServlet extends javax.servlet.http.HttpServlet {
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
             try{
-                response.getWriter().println("helloasdsaasd servlet");
+                response.getWriter().println("helloaswdsaasd servlet");
                 response.getWriter().println(new Date().toLocaleString());
             }catch (Exception e){
                 e.printStackTrace();
